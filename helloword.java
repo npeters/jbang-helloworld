c3bd839d699a473796868f80ccb5355f73eb3b4e
Nicolas Peters <nicolas.peters@canal-plus.com>
2021-06-17 16:41:48 +0200

    add Doom Font

--- helloword.java
+++ helloword.java
@@ -1,11 +1,32 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-// //DEPS <dependency1> <dependency2>
+//DEPS info.picocli:picocli:4.5.0 info.picocli:picocli-codegen:4.6.1
 
+//DEPS com.github.lalyos:jfiglet:0.0.8
+//FILES Doom.flf
+
+//JAVA 16
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 import static java.lang.System.*;
+import java.util.concurrent.Callable;
 
-public class helloword {
+@Command(name = "helloword", mixinStandardHelpOptions = true, version = "helloword 0.1", description = "helloword made with jbang")
+class helloword implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
+    private String greeting;
 
     public static void main(String... args) {
-        out.println("Hello World");
+        int exitCode = new CommandLine(new helloword()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        java.lang.System.out.println(
+                com.github.lalyos.jfiglet.FigletFont.convertOneLine(new java.io.File("./Doom.flf"), "Hello PJUG "));
+        return 0;
     }
 }
